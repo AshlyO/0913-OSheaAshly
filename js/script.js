1e424a2552cd64da828efbf32ecb43f9cacb0053
Ashly <ashlyoshea@fullsail.edu>
2013-09-12 22:43:17 -0400

    failing to see proper code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ashlyoshea
- * Date: 9/12/13
- * Time: 10:27 PM
- * To change this template use File | Settings | File Templates.
- */
+// JavaScript
+
+                var yearBorn = 1980
+consol.log (var yearBorn = 1980;)
